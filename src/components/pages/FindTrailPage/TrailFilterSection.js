1506b8d1aefb1f2f8d298effb3c4f1de81362346
Mikaela Gurney <354@holbertonschool.com>
2018-12-26 02:25:36 -0800

    Create trail filters section

--- src/components/pages/FindTrailPage/TrailFilterSection.js
+++ src/components/pages/FindTrailPage/TrailFilterSection.js
@@ -1,5 +1,32 @@
 import React from "react";
 
-export default function TrailFilterSection() {
-  return <div className="trail-filter-section" />;
-}
+const TrailFilterSection = () => (
+  <div className="trail-filter-section">
+    <form className="trail-filter-form">
+      <label className="trail-filter__distance">
+        Distance to trail
+        <input type="range" name="maxDistance" min="30" max="200" />
+      </label>
+      <label className="trail-filter__maxResults">
+        Max Results
+        <input type="range" name="maxResults" min="10" max="500" />
+      </label>
+      <label className="trail-filter__minLength">
+        Minimum Trail Length
+        <input type="range" name="maxResults" min="0" max="500" />
+      </label>
+      <label className="trail-filter__starRating">
+        Minimum Star Rating
+        <input type="range" name="minStars" min="0" max="4" />
+      </label>
+
+      <input
+        className="trail-filter__submit-button"
+        type="submit"
+        value="Submit"
+      />
+      <input className="trail-filter__reset-button" type="reset" />
+    </form>
+  </div>
+);
+export default TrailFilterSection;
